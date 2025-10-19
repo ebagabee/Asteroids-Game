3c922d5fd2a274af89fd5d51b9f51b9ae6d18ce2
ebagabee <ebagabe.2025@gmail.com>
2025-10-19 09:04:25 -0300

    refactor: cleanup the functions

--- shot.py
+++ shot.py
@@ -3,8 +3,8 @@ import pygame
 from constants import SHOT_RADIUS
 
 class Shot(CircleShape):
-    def __init__(self, x, y, radius):
-        super().__init__(x, y, radius)
+    def __init__(self, x, y):
+        super().__init__(x, y, SHOT_RADIUS)
 
     def draw(self, screen):
         pygame.draw.circle(screen, "white", self.position, self.radius, SHOT_RADIUS)
